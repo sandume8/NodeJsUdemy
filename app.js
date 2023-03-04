64e6a8eae7844b50c8fe5def394a560ef1713fe6
sandume8 <eunju.kwak@konantech.com>
2023-03-04 15:40:21 +0900

    6강 ejs template engine

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ const adminData = require('./routes/admin');
 const shopRouters = require('./routes/shop');
 
 const app = express();
-// pug와 다르게 외부 engine일 경우 express에 engine 등록, 첫번째 인자가 views에서 사용할 확장자 명
-app.engine(
-    'hbs',
-    expressHbs({
-        layoutsDir:'views/layouts/',
-        defaultLayout:'main-layout',
-        extname:'hbs'
-    })
-);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+// pug와 다르게 외부 engine일 경우 express에 engine 등록, extname: views에서 사용할 확장자 명
+// app.engine(
+//     'hbs',
+//     expressHbs({
+//         layoutsDir:'views/layouts/',
+//         defaultLayout:'main-layout',
+//         extname:'hbs'
+//     })
+// );
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 // app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,7 +30,7 @@ app.use(shopRouters);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.status(404).render('404', {pageTitle:'Page Not Found'})
+    res.status(404).render('404', {pageTitle:'Page Not Found', path:'Error'})
 });
 
 app.listen(3000);
